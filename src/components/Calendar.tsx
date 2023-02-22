2fc689028f11d6bcf43048ecae4522abbcbf803f
jaren <jaren123@students.ubc.ca>
2023-02-21 16:22:18 -0800

    create grid of months, need to calculate days

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Box, Stack, Divider } from "@mui/material/";
 import Grid2 from "@mui/material/Unstable_Grid2";
+import DayMonthView from "@/components/calendarComponents/DayMonthView";
 
 const Calendar = () => {
   return (
     <Box
-      bgcolor="white"
+      bgcolor="beige"
       color="black"
       flex={1}
       sx={{ height: "calc(100vh - 64px)" }}
+      border={1}
     >
       Calendar
+      <DayMonthView />
+      
     </Box>
   );
 };
--- src/components/calendarComponents/DayMonthView.tsx
+++ src/components/calendarComponents/DayMonthView.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Box, Stack, Divider} from '@mui/material/';
-import Grid2 from '@mui/material/Unstable_Grid2';
+import Grid from '@mui/material/Unstable_Grid2';
 
+const DayMonthView = () => {
+    const [currentDate, setDate] = useState(new Date());
 
-// const DayMonthView = () => {
-//     return (
-//     )
-// };
+    const months = [    "January",    "February",    "March",    "April",    "May",    "June",    "July",    "August",    "September",    "October",    "November",    "December"  ];
+    const days = [    "Sunday",    "Monday",    "Tuesday",    "Wednesday",    "Thursday",    "Friday",    "Saturday"  ];
 
-//export default DayMonthView;
+    function getNextMonth(){
+        setDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1));
+    }
+
+    function getPrevMonth(){
+        setDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1));
+    }
+
+    return (
+        <Grid container 
+        columns={7} 
+        spacing={0} 
+        border = {1} 
+        alignItems="stretch" 
+        sx={{height:'95%'}}>
+            {Array.from(Array(35)).map((_, index) => (
+            <Grid key={index} sx={{height:'auto'}}
+                xs={1} 
+                border = {1} 
+                display="flex" 
+                justifyContent="center" alignItems="top" >
+                {index + 1}
+            </Grid>
+            ))}
+        </Grid>
+    )
+};
+
+export default DayMonthView;
