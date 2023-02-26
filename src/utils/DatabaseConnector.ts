cd5d241eb11a4ac6b3a611abe57b4652fbdfe7f4
rcacho <rcacho@hotmail.ca>
2023-02-25 19:03:03 -0800

    use env for connection

--- src/utils/DatabaseConnector.ts
+++ src/utils/DatabaseConnector.ts
@@ -1,30 +1,26 @@
 const sql = require('mssql');
 
 export class DatabaseConnector {
-    configs = {
-        user: 'CloudSA2a696802', 
-        password: 'O62Fsn%*l7t7', 
-        server: 'calendar-server-db-dev.database.windows.net', 
-        database: 'calendar-db-dev', 
-        authentication: {
-            type: 'default'
-        },
-        options: {
-            encrypt: true
-        }
-    };
+  configs = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_NAME,
+    authentication: {
+      type: 'default',
+    },
+    options: {
+      encrypt: true,
+    },
+  };
 
-    async ConnectAndQuery(query: String) {
-        try {
-            const poolConnection = await sql.connect(this.configs); 
-            // const resultSet = await connection.query(query); 
-            // return resultSet; 
-            console.log("Connection created");
-            const resultSet = await poolConnection.request().query(query);
-            console.log(resultSet);
-            return resultSet;
-        } catch (err: any) {
-            console.error(err.stack);
-        }
+  async ConnectAndQuery(query: String) {
+    try {
+      const poolConnection = await sql.connect(this.configs);
+      const resultSet = await poolConnection.request().query(query);
+      return resultSet;
+    } catch (err: any) {
+      console.error(err.stack);
     }
+  }
 }
