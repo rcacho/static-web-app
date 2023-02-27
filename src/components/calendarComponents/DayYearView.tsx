81a5aeebd5652d87535da229926cd61378621913
jaren <jaren123@students.ubc.ca>
2023-02-27 13:40:58 -0800

    fit months to year view, need to implement the different months now

--- src/components/calendarComponents/DayYearView.tsx
+++ src/components/calendarComponents/DayYearView.tsx
@@ -5,8 +5,52 @@ import {Box, Stack, Divider} from '@mui/material/';
 import Grid from '@mui/material/Unstable_Grid2';
 import DayMonthView from './DayMonthView';
 
+// slightly different from DayMonthView component
+function GenerateMonth(props) {
+    const smallDays = ["Su", "M", "T", "W", "Th", "F", "S"]
+    let day: Number = props.firstDay(props.currentDate) - 1;
+    return (
+        <Grid container 
+        columns={7} 
+        spacing={0} 
+        border = {0}
+        alignItems="stretch" 
+        textAlign={"center"}
+        sx={{height:'auto'}}>
+
+            {Array.from(Array(42)).map((_, index) => (
+            ((index - day > 0 && index - day <= props.daysInMonth(props.currentDate)) || index < 7 ? 
+            <Grid key={index - day} 
+                sx={{height:'auto'}}
+                xs={1} 
+                display="flex" 
+                justifyContent="center" 
+                alignItems="top" >
+                    {/* 
+                        will probably need to change this to another inner grid 
+                        so that we can add some events/icons to the dates
+                    */}
+                <Typography fontSize={"100%"}>
+                {(index < 7 ? smallDays[index] : "")} 
+                {(index < 7 ? <br/> : "")}
+                
+                <Button size = "small" style={{fontSize: '80%', color: 'black'}} >
+                {(index - day > 0 && index - day <= props.daysInMonth(props.currentDate) ? index - day : " ")}
+                </Button>
+                </Typography>
+            </Grid> :
+            ""
+            )
+            
+            ))}
+
+        </Grid>
+    )
+}
+
 const DayYearView = (props) => {
     let day: Number = props.firstDay(props.currentDate) - 1;
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     
     return (
         <Grid container 
@@ -14,29 +58,28 @@ const DayYearView = (props) => {
         spacing={0} 
         border = {0} 
         alignItems="stretch" 
+        display = "flex"
         sx={{height:'100%'}}>
 
             {Array.from(Array(12)).map((_, index) => (
-            <Grid key={index - day} 
-                sx={{height: 'auto'}}
-                xs={1} 
-                borderRight = {1} 
-                borderBottom = {1} 
-                display="flex" 
-                justifyContent="center" 
-                alignItems="center" >
+            <Grid 
+            xs = {1}
+            border = {0}
+            >
+                <Button style={{color: "black"}}>{months[index]}</Button>
 
-                <DayMonthView 
+                <GenerateMonth
                 firstDay = {props.firstDay} 
                 daysInMonth = {props.daysInMonth} 
                 days = {props.days}
                 currentDate = {props.currentDate}/>
-
+                    
             </Grid>
-            ))}
 
+        ))}
         </Grid>
     )
+        
 };
 
 export default DayYearView;
