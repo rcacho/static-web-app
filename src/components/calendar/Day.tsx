43867c3b89d8f90df2b63e16dc38d0fcc51580f8
JuliaS96 <76749862+JuliaS96@users.noreply.github.com>
2023-03-12 20:25:03 -0700

    added symbols to month view

--- src/components/calendar/Day.tsx
+++ src/components/calendar/Day.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Typography, Button } from '@mui/material'
 import Grid from '@mui/material/Unstable_Grid2'
 import { useCalendarContext } from '@/store/CalendarContext'
+import CircleIcon from '@mui/icons-material/Circle'
 
 export const noValue = ''
 
@@ -13,7 +14,50 @@ interface DayProps {
 }
 
 const Day = (props: DayProps) => {
+  const IconList = [
+    <CircleIcon fontSize="small" style={{ fill: '#0072ea' }}></CircleIcon>,
+    <CircleIcon fontSize="small" style={{ fill: 'red' }}></CircleIcon>,
+    <CircleIcon fontSize="small" style={{ fill: 'purple' }}></CircleIcon>,
+    <CircleIcon fontSize="small" style={{ fill: '#0072ea' }}></CircleIcon>,
+    <CircleIcon fontSize="small" style={{ fill: '#81ea00' }}></CircleIcon>,
+    <CircleIcon fontSize="small" style={{ fill: '#ea6900' }}></CircleIcon>,
+    <CircleIcon fontSize="small" style={{ fill: '#ea00cb' }}></CircleIcon>,
+    <CircleIcon fontSize="small" style={{ fill: 'black' }}></CircleIcon>,
+    <CircleIcon fontSize="small" style={{ fill: '#00ea37' }}></CircleIcon>,
+    <CircleIcon fontSize="small" style={{ fill: '#ea9800' }}></CircleIcon>,
+    <CircleIcon fontSize="small" style={{ fill: '#81ea00' }}></CircleIcon>,
+    <CircleIcon fontSize="small" style={{ fill: '#ea6900' }}></CircleIcon>,
+    <CircleIcon fontSize="small" style={{ fill: '#ea00cb' }}></CircleIcon>,
+    <CircleIcon fontSize="small" style={{ fill: 'black' }}></CircleIcon>,
+    <CircleIcon fontSize="small" style={{ fill: '#00ea37' }}></CircleIcon>,
+    <CircleIcon fontSize="small" style={{ fill: '#ea9800' }}></CircleIcon>
+  ]
   const { isYearView } = useCalendarContext()
+
+  function ReturnGrid() {
+    if (IconList.length < 12) {
+      return (
+        <Grid container>
+          {IconList.map((icon, index) => (
+            <Grid key={index} xs={2}>
+              {icon}
+            </Grid>
+          ))}
+        </Grid>
+      )
+    } else {
+      return (
+        <Grid container>
+          {IconList.slice(0, 11).map((icon, index) => (
+            <Grid key={index} xs={2}>
+              {icon}
+            </Grid>
+          ))}
+          <Grid xs={2}>+{IconList.length - 11}</Grid>
+        </Grid>
+      )
+    }
+  }
 
   const renderDayOfWeek = () => {
     if (props.dayOfWeek != noValue) {
@@ -27,22 +71,46 @@ const Day = (props: DayProps) => {
   }
 
   const renderDate = () => {
-    return props.day == 0 ? (
-      <></>
-    ) : (
-      <Button
-        onClick={() => props.handleDayClick(props.day)}
-        size={isYearView ? 'small' : 'large'}
-        style={{
-          fontSize: isYearView ? '100%' : '24px',
-          color: '#4D4D4D',
-          maxWidth: isYearView ? '30px' : '60px',
-          minWidth: isYearView ? '30px' : '60px'
-        }}
-      >
-        {props.day.toString()}
-      </Button>
-    )
+    if (props.day === 0) {
+      return <></>
+    } else if (isYearView) {
+      return (
+        <Button
+          onClick={() => props.handleDayClick(props.day)}
+          size={isYearView ? 'small' : 'large'}
+          style={{
+            fontSize: isYearView ? '100%' : '24px',
+            color: '#4D4D4D',
+            maxWidth: isYearView ? '30px' : '60px',
+            minWidth: isYearView ? '30px' : '60px'
+          }}
+        >
+          {props.day.toString()}
+        </Button>
+      )
+    } else {
+      return (
+        <>
+          <Grid container>
+            <Grid xs={12}>
+              <Button
+                onClick={() => props.handleDayClick(props.day)}
+                size={isYearView ? 'small' : 'large'}
+                style={{
+                  fontSize: isYearView ? '100%' : '24px',
+                  color: '#4D4D4D',
+                  maxWidth: isYearView ? '30px' : '60px',
+                  minWidth: isYearView ? '30px' : '60px'
+                }}
+              >
+                {props.day.toString()}
+              </Button>
+            </Grid>
+            <ReturnGrid></ReturnGrid>
+          </Grid>
+        </>
+      )
+    }
   }
 
   return (
