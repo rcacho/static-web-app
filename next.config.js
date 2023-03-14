faeee1cc949062ead979e9cfb1e6189c2effd8ef
rcacho <rcacho@hotmail.ca>
2023-03-13 22:28:31 -0700

    clean up env

--- next.config.js
+++ next.config.js
@@ -4,13 +4,10 @@ require('dotenv').config()
 
 module.exports = {
   env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     AZURE_AD_B2C_CLIENT_ID: process.env.AZURE_AD_B2C_CLIENT_ID,
     AZURE_AD_B2C_TENANT_NAME: process.env.AZURE_AD_B2C_TENANT_NAME,
     AZURE_AD_B2C_PRIMARY_USER_FLOW: process.env.AZURE_AD_B2C_PRIMARY_USER_FLOW,
     AZURE_AD_B2C_PROFILE_EDIT: process.env.AZURE_AD_B2C_PROFILE_EDIT,
-    JWT_SECRET: process.env.JWT_SECRET,
- 
   },
   reactStrictMode: true
 }
