adbc0ab22a124b2cef1067f905cb873cb9dd1010
chrismitchellcs <chrismitchellcs@gmail.com>
2023-03-15 21:45:06 -0700

    added backend connection for legend

--- src/components/legendComponents/CategoryList.tsx
+++ src/components/legendComponents/CategoryList.tsx
@@ -1,3 +1,4 @@
+import { Category } from '@/interfaces/Category'
 import { useCalendarContext } from '@/store/CalendarContext'
 import {
   Avatar,
@@ -6,18 +7,104 @@ import {
   Checkbox,
   List,
   ListItem,
-  ListItemAvatar,
   ListItemButton,
+  ListItemIcon,
   ListItemText
 } from '@mui/material'
+import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined'
+import CloseIcon from '@mui/icons-material/Close'
+import HexagonOutlinedIcon from '@mui/icons-material/HexagonOutlined'
+import SquareOutlinedIcon from '@mui/icons-material/SquareOutlined'
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined'
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined'
+import SquareIcon from '@mui/icons-material/Square'
+import GroupsIcon from '@mui/icons-material/Groups'
+import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule'
+import CodeIcon from '@mui/icons-material/Code'
+import CropIcon from '@mui/icons-material/Crop'
+import CloudOutlinedIcon from '@mui/icons-material/CloudOutlined'
 
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import axios from 'axios'
+import { APIManager } from '@/utils/APIManager'
 
 const CategoryList = () => {
-  const { events, selected, handleAll, handleNone, handleChange } =
-    useCalendarContext()
+  const {
+    selected,
+    categories,
+    setCategories,
+    handleAll,
+    handleNone,
+    handleChange
+  } = useCalendarContext()
+
+  const icons = {
+    CircleOutlinedIcon,
+    CloseIcon,
+    HexagonOutlinedIcon,
+    SquareOutlinedIcon,
+    KeyboardArrowUpOutlinedIcon,
+    StarBorderOutlinedIcon,
+    SquareIcon,
+    GroupsIcon,
+    HorizontalRuleIcon,
+    CodeIcon,
+    CropIcon,
+    CloudOutlinedIcon
+  }
+
+  // useEffect(() => {
+  //   APIManager.getInstance()
+  //     .then((instance: any) => instance.getCategory())
+  //     .then((data) => {
+  //       let records = JSON.parse(data)
+  //       let categories: Category[] = records.recordset
+  //       console.log(categories)
+  //     })
+  //     .catch((err) => {
+  //       // console.log(err)
+  //     })
+  // }, [])
+
+  // const getCategories = () => {
+  //   APIManager.getInstance()
+  //     .then((instance: any) => instance.getCategory())
+  //     .then((data) => {
+  //       categories = data
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //     })
+  // }
+
+  // useEffect(() => {
+  //   APIManager.getInstance()
+  //     .then((instance: any) => instance.getCategory())
+  //     .then((data) => {
+  //       categories = data
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //     })
+  // })
+  // const [categories, setCategories] = React.useState<Category[]>([])
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const instance = await APIManager.getInstance()
+  //     const data = await instance.getCategory()
+  //     const cat: Category[] = data.result
+  //     console.log(cat)
+  //     setCategories(cat)
+  //     // console.log(categories)
+  //   }
+  //   getData().catch((err) => {
+  //     console.log(err)
+  //   })
+  // }, [])
+
   return (
-    <Box height="calc(100vh - 64px)">
+    <Box>
       <Button
         onClick={handleAll}
         sx={[
@@ -51,16 +138,17 @@ const CategoryList = () => {
 
       <List
         dense
-        style={{ overflow: 'auto', height: 'calc(100vh - 64px - 60px' }}
+        style={{ overflow: 'auto' }}
         sx={{ bgcolor: 'background.paper' }}
       >
-        {events.map((item: any) => {
-          const labelId = `checkbox-list-secondary-label-${item}`
+        {categories.map((item: Category) => {
+          const labelId = `checkbox-list-secondary-label-${item.category_name}`
+          const Icon = icons[item.icon]
           return (
             <ListItem
               secondaryAction={
                 <Checkbox
-                  value={item}
+                  value={item.category_name}
                   edge="end"
                   onChange={handleChange}
                   checked={selected.includes(item)}
@@ -70,10 +158,11 @@ const CategoryList = () => {
               disablePadding
             >
               <ListItemButton>
-                <ListItemAvatar>
-                  <Avatar alt={`AE`} src={`placeholder`} />
-                </ListItemAvatar>
-                <ListItemText id={labelId} primary={`${item}`} />
+                <ListItemIcon>
+                  {/* <Avatar alt={`AE`} src={`placeholder`} /> */}
+                  <Icon sx={{ color: item.color }} />
+                </ListItemIcon>
+                <ListItemText id={labelId} primary={`${item.category_name}`} />
               </ListItemButton>
             </ListItem>
           )
