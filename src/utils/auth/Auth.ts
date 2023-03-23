1f9e69f33e1b70ef2638c9647287e8ce3c81d5f1
rcacho <rcacho@hotmail.ca>
2023-03-22 18:11:36 -0700

    add auth middleware

--- src/utils/auth/Auth.ts
+++ src/utils/auth/Auth.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { IncomingHttpHeaders } from 'http'
 import { decode, JwtPayload, verify } from 'jsonwebtoken'
 import { JwksClient } from 'jwks-rsa'
+import { label, Middleware } from 'next-api-middleware'
 
 function getSigningKeyPromise(kid: string, client: JwksClient) {
   return new Promise<string>((resolve, reject) => {
@@ -26,45 +26,47 @@ function getSigningKeyPromise(kid: string, client: JwksClient) {
   })
 }
 
-export async function authenticate(req: NextApiRequest, res: NextApiResponse) {
-  if (
-    !req.headers.authorization ||
-    !req.headers.authorization.startsWith('Bearer ')
-  ) {
-    res.status(401).json({ result: 'Missing bearer token' })
-  }
+const authenticate: Middleware = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  next: () => Promise<void>
+) => {
+  try {
+    if (
+      !req.headers.authorization ||
+      !req.headers.authorization.startsWith('Bearer ')
+    ) {
+      throw new Error('Missing bearer token')
+    }
 
-  const idToken = req.headers.authorization!.substring(7)
+    const idToken = req.headers.authorization!.substring(7)
 
-  const parsed = decode(idToken, { complete: true })
+    const parsed = decode(idToken, { complete: true })
 
-  console.log('PARSED: ', parsed)
+    const kid = parsed?.header.kid
+    if (!kid) {
+      throw new Error('Malformed IdToken')
+    }
 
-  const kid = parsed?.header.kid
-  if (!kid) {
-    res.status(401).json({ result: 'Malformed IdToken' })
-  }
+    const client = new JwksClient({
+      jwksUri:
+        'https://t3am319.b2clogin.com/t3am319.onmicrosoft.com/B2C_1_SISOPolicy/discovery/v2.0/keys'
+    })
 
-  const client = new JwksClient({
-    jwksUri:
-      'https://t3am319.b2clogin.com/t3am319.onmicrosoft.com/B2C_1_SISOPolicy/discovery/v2.0/keys'
-  })
+    let signingKey = await getSigningKeyPromise(kid!, client)
 
-  let signingKey = await getSigningKeyPromise(kid!, client)
+    const decodedAndVerified = verify(idToken, signingKey) as JwtPayload
 
-  const decodedAndVerified = verify(idToken, signingKey) as JwtPayload
+    if (decodedAndVerified.aud !== process.env.AZURE_AD_B2C_CLIENT_ID) {
+      throw new Error('The authentication is for the wrong application')
+    }
 
-  console.log('DECODED:', decodedAndVerified)
-
-  const exp = Number(decodedAndVerified.exp!)
-
-  if (new Date(exp * 1000) < new Date()) {
-    res.status(401).json({ result: 'Expired authentication' })
-  }
-
-  if (decodedAndVerified.aud !== process.env.AZURE_AD_B2C_CLIENT_ID) {
-    res
-      .status(401)
-      .json({ result: 'The authentication is for the wrong application' })
+    next()
+  } catch (err) {
+    res.status(401).json({ result: err })
   }
 }
+
+export const withAuthMiddleware = label({
+  authenticate
+})
