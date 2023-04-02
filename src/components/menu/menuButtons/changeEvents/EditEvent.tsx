938ca03bdcae5321e64b3d694d0941c1695b9e11
sigani <82123942+sigani@users.noreply.github.com>
2023-04-02 00:13:45 -0700

    attempted edit event fix again

--- src/components/menu/menuButtons/changeEvents/EditEvent.tsx
+++ src/components/menu/menuButtons/changeEvents/EditEvent.tsx
@@ -50,6 +50,7 @@ const EditEvent = (props: any) => {
   const [eventDate, setEventDate] = useState(nullDate)
   const [description, setEventDescription] = useState('')
   const [oldDate, setOldDate] = useState(nullDate)
+  const [oldCat, setOldCat] = useState(-1)
 
   const adminID = accountId
 
@@ -65,6 +66,7 @@ const EditEvent = (props: any) => {
       setSelected(catIDs.indexOf(selectedEvent))
       setEventDate(new Date(reformatDate(selectedDate as string)))
       setOldDate(new Date(reformatDate(selectedDate as string)))
+      setOldCat(selectedEvent)
       setFirst(false)
     }
     if (props.fromMenu === 1) setEventDate(nullDate)
@@ -106,7 +108,8 @@ const EditEvent = (props: any) => {
             let eDate = new Date(data.result[i].event_date)
             if (eDate.toUTCString() === eventDate.toUTCString()) {
               if (data.result[i].category_id === catIDs[selected]) {
-                if (+oldDate !== +eventDate) {
+                // if the date and cat hasn't changed, do the edit
+                if (+oldDate === +eventDate && oldCat === catIDs[selected]) {
                   break
                 } else {
                   setOpenFailed(true)
