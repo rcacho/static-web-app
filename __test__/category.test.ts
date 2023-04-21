7461f8b48bbae7106690828f890e8f9d589ceb37
rcacho <rcacho@hotmail.ca>
2023-04-20 20:59:19 -0700

    working tests

--- __test__/category.test.ts
+++ __test__/category.test.ts
@@ -22,11 +22,7 @@ describe('Test Category API endpoints', () => {
   const nonAdminToken = `Bearer ${process.env.NON_ADMIN_JWT_TOKEN}`
   const adminToken = `Bearer ${process.env.ADMIN_JWT_TOKEN}`
 
-  beforeAll(async () => {
-    db = new DatabaseConnector()
-  })
-
-  afterAll(async () => {
+  const cleanUp = async (db: DatabaseConnector) => {
     const query = `
     DELETE FROM calendar.category 
     WHERE user_id = 
@@ -35,7 +31,54 @@ describe('Test Category API endpoints', () => {
       WHERE active_directory_oid = '${process.env.ADMIN_OID}')`
 
     await db.ConnectAndQuery(query)
+  }
+
+  beforeAll(async () => {
+    db = new DatabaseConnector()
+    await cleanUp(db)
   })
+
+  afterAll(async () => {
+    await cleanUp(db)
+  })
+
+  const createCategory = async () => {
+    const postHandler: NextApiHandler<any> = category
+    const category_name = 'Test' + generateRandomString(8)
+
+    // create a new category for use in tests
+    await testApiHandler({
+      handler: postHandler,
+      test: async ({ fetch }) => {
+        const res = await fetch({
+          method: 'POST',
+          headers: {
+            Authorization: adminToken,
+            'content-type': 'application/json'
+          },
+          body: JSON.stringify({
+            category_name,
+            color: 'yellow',
+            icon: 'CircleIcon'
+          })
+        })
+        expect(res.status).toBe(200)
+      }
+    })
+
+    const res = await db.ConnectAndQuery(
+      `SELECT id FROM calendar.category WHERE name = '${category_name}'`
+    )
+
+    const id = res.recordset[0].id
+
+    return {
+      category_id: id,
+      category_name,
+      color: 'yellow',
+      icon: 'CircleIcon'
+    }
+  }
 
   it('test GET - /category', async () => {
     const handler = category
@@ -136,24 +179,24 @@ describe('Test Category API endpoints', () => {
   it('test PUT - /category/id', async () => {
     const handler = categoryid
 
-    const categoryId = 374
-
-    const category_name = 'Statutory Holiday'
-    const color = 'red'
-    const icon = 'CircleIcon'
+    const category = await createCategory()
 
     const body = {
-      category_name,
-      color,
-      icon
+      category_name: category.category_name,
+      color: 'orange',
+      icon: category.icon
     }
 
     // unauthenticated request
     await testApiHandler({
       handler,
-      paramsPatcher: (params) => (params.id = categoryId),
+      paramsPatcher: (params) => (params.id = category.category_id),
       test: async ({ fetch }) => {
-        const res = await fetch({ method: 'PUT', body: body })
+        const res = await fetch({
+          method: 'PUT',
+          headers: { 'content-type': 'application/json' },
+          body: JSON.stringify(body)
+        })
         expect(res.status).toBe(401)
       }
     })
@@ -161,12 +204,15 @@ describe('Test Category API endpoints', () => {
     // non-admin authenticate request
     await testApiHandler({
       handler,
-      paramsPatcher: (params) => (params.id = categoryId),
+      paramsPatcher: (params) => (params.id = category.category_id),
       test: async ({ fetch }) => {
         const res = await fetch({
           method: 'PUT',
-          body: body,
-          headers: { Authorization: nonAdminToken }
+          body: JSON.stringify(body),
+          headers: {
+            Authorization: nonAdminToken,
+            'content-type': 'application/json'
+          }
         })
         expect(res.status).toBe(401)
       }
@@ -175,63 +221,46 @@ describe('Test Category API endpoints', () => {
     // admin authenticate request
     await testApiHandler({
       handler,
-      paramsPatcher: (params) => (params.id = categoryId),
+      paramsPatcher: (params) => (params.id = category.category_id),
       test: async ({ fetch }) => {
         const res = await fetch({
           method: 'PUT',
-          body: body,
-          headers: { Authorization: adminToken }
-        })
-        expect(res.status).toBe(200)
-      }
-    })
-  })
-
-  it.only('test DELETE - /category/id', async () => {
-    const postHandler: NextApiHandler<any> = category
-    const handler = categoryid
-
-    const category_name = 'Test' + generateRandomString(8)
-
-    // create a new category for use in tests
-    await testApiHandler({
-      handler: postHandler,
-      test: async ({ fetch }) => {
-        const res = await fetch({
-          method: 'POST',
+          body: JSON.stringify(body),
           headers: {
             Authorization: adminToken,
             'content-type': 'application/json'
-          },
-          body: JSON.stringify({
-            category_name,
-            color: 'yellow',
-            icon: 'CircleIcon'
-          })
+          }
         })
         expect(res.status).toBe(200)
       }
     })
 
-    const res = await db.ConnectAndQuery(
-      `SELECT id FROM calendar.category WHERE name = '${category_name}'`
+    const putRes = await db.ConnectAndQuery(
+      `SELECT color FROM calendar.category WHERE name = '${body.category_name}'`
     )
 
-    const categoryId = res.recordset[0].id
+    expect(putRes.recordset[0].color).toBe(body.color)
+  })
+
+  it('test DELETE - /category/id', async () => {
+    const handler = categoryid
+
+    const category = await createCategory()
 
     // unauthenticated request
     await testApiHandler({
       handler,
-      paramsPatcher: (params) => (params.id = categoryId),
+      paramsPatcher: (params) => (params.id = category.category_id),
       test: async ({ fetch }) => {
         const res = await fetch({ method: 'DELETE' })
         expect(res.status).toBe(401)
       }
     })
 
+    // non-admin authenticated request
     await testApiHandler({
       handler,
-      paramsPatcher: (params) => (params.id = categoryId),
+      paramsPatcher: (params) => (params.id = category.category_id),
       test: async ({ fetch }) => {
         const res = await fetch({
           method: 'DELETE',
@@ -241,9 +270,10 @@ describe('Test Category API endpoints', () => {
       }
     })
 
+    // admin authenticated request
     await testApiHandler({
       handler,
-      paramsPatcher: (params) => (params.id = categoryId),
+      paramsPatcher: (params) => (params.id = category.category_id),
       test: async ({ fetch }) => {
         const res = await fetch({
           method: 'DELETE',
@@ -254,7 +284,7 @@ describe('Test Category API endpoints', () => {
     })
 
     const deleteRes = await db.ConnectAndQuery(
-      `SELECT COUNT(*) AS count FROM calendar.category WHERE name = '${category_name}'`
+      `SELECT COUNT(*) AS count FROM calendar.category WHERE name = '${category.category_name}'`
     )
 
     expect(deleteRes.recordset[0].count).toBe(0)
--- __test__/event.test.ts
+++ __test__/event.test.ts
@@ -1,6 +1,6 @@
 import { DatabaseConnector } from '@/utils/DatabaseConnector'
 import event from '../src/pages/api/event'
-// import eventid from '../src/pages/api/event/[id]'
+import eventid from '../src/pages/api/event/[id]'
 import { testApiHandler } from 'next-test-api-route-handler'
 import { CategoryDAO } from '@/utils/dao/CategoryDAO'
 import { icons } from '@/interfaces/Icons'
@@ -50,14 +50,55 @@ describe('Test Event API Endpoints', () => {
 
   afterAll(async () => {
     const query = `
-	DELETE FROM calendar.category 
-	WHERE user_id = 
-	  (SELECT id 
-	  FROM calendar.app_user
-	  WHERE active_directory_oid = '${adminOID}')`
+	  DELETE FROM calendar.category 
+	  WHERE user_id = 
+	    (SELECT id 
+	    FROM calendar.app_user
+	    WHERE active_directory_oid = '${adminOID}')`
 
     await db.ConnectAndQuery(query)
   })
+
+  const createEvent = async (dateString: string) => {
+    const handler = event
+
+    const e = {
+      event_date: dateString,
+      event_description: 'this is the nightmail',
+      category_id: baseCategoryId,
+      event_id: null
+    }
+
+    await testApiHandler({
+      handler,
+      test: async ({ fetch }) => {
+        const res = await fetch({
+          method: 'POST',
+          headers: {
+            Authorization: adminToken,
+            'content-type': 'application/json'
+          },
+          body: JSON.stringify(e)
+        })
+        expect(res.status).toBe(200)
+      }
+    })
+
+    const res = await db.ConnectAndQuery(
+      `SELECT id FROM calendar.event 
+       WHERE event_date = '${e.event_date}' 
+       AND category_id = ${baseCategoryId} `
+    )
+
+    const id = res.recordset[0].id
+
+    return {
+      id: id,
+      date: e.event_date,
+      description: e.event_description,
+      category_id: e.category_id
+    }
+  }
 
   it('test GET - /event', async () => {
     const handler = event
@@ -136,9 +177,135 @@ describe('Test Event API Endpoints', () => {
         expect(res.status).toBe(200)
       }
     })
+
+    // try to duplicate category
+    await testApiHandler({
+      handler,
+      test: async ({ fetch }) => {
+        const res = await fetch({
+          method: 'POST',
+          headers: {
+            Authorization: adminToken,
+            'content-type': 'application/json'
+          },
+          body: JSON.stringify(e)
+        })
+        expect(res.status).toBe(409)
+      }
+    })
   })
 
-  it('test PUT - /event/id', async () => {})
+  it('test PUT - /event/id', async () => {
+    const handler = eventid
 
-  it('test DELETE - /category/id', async () => {})
+    const event = await createEvent('2000-01-02')
+
+    const body = {
+      event_date: event.date,
+      event_description: "it's gonna rain",
+      category_id: event.category_id
+    }
+
+    // unauthenticated request
+    await testApiHandler({
+      handler,
+      paramsPatcher: (params) => (params.id = event.id),
+      test: async ({ fetch }) => {
+        const res = await fetch({
+          method: 'PUT',
+          body: JSON.stringify(body),
+          headers: { 'content-type': 'application/json' }
+        })
+        expect(res.status).toBe(401)
+      }
+    })
+
+    // non-admin authenticate request
+    await testApiHandler({
+      handler,
+      paramsPatcher: (params) => (params.id = event.id),
+      test: async ({ fetch }) => {
+        const res = await fetch({
+          method: 'PUT',
+          body: JSON.stringify(body),
+          headers: {
+            Authorization: nonAdminToken,
+            'content-type': 'application/json'
+          }
+        })
+        expect(res.status).toBe(401)
+      }
+    })
+
+    // admin authenticate request
+    await testApiHandler({
+      handler,
+      paramsPatcher: (params) => (params.id = event.id),
+      test: async ({ fetch }) => {
+        const res = await fetch({
+          method: 'PUT',
+          body: JSON.stringify(body),
+          headers: {
+            Authorization: adminToken,
+            'content-type': 'application/json'
+          }
+        })
+        expect(res.status).toBe(200)
+      }
+    })
+
+    const putRes = await db.ConnectAndQuery(
+      `SELECT event_description FROM calendar.event WHERE id = ${event.id}`
+    )
+
+    expect(putRes.recordset[0].event_description).toBe(body.event_description)
+  })
+
+  it('test DELETE - /event/id', async () => {
+    const handler = eventid
+
+    const event = await createEvent('2010-01-01')
+
+    // unauthenticated request
+    await testApiHandler({
+      handler,
+      paramsPatcher: (params) => (params.id = event.id),
+      test: async ({ fetch }) => {
+        const res = await fetch({ method: 'DELETE' })
+        expect(res.status).toBe(401)
+      }
+    })
+
+    // non-admin authenticated request
+    await testApiHandler({
+      handler,
+      paramsPatcher: (params) => (params.id = event.id),
+      test: async ({ fetch }) => {
+        const res = await fetch({
+          method: 'DELETE',
+          headers: { Authorization: nonAdminToken }
+        })
+        expect(res.status).toBe(401)
+      }
+    })
+
+    // admin authenticated request
+    await testApiHandler({
+      handler,
+      paramsPatcher: (params) => (params.id = event.id),
+      test: async ({ fetch }) => {
+        const res = await fetch({
+          method: 'DELETE',
+          headers: { Authorization: adminToken }
+        })
+        expect(res.status).toBe(200)
+      }
+    })
+
+    const deleteRes = await db.ConnectAndQuery(
+      `SELECT is_deleted AS deleted FROM calendar.event WHERE id = ${event.id}`
+    )
+
+    expect(deleteRes.recordset[0].deleted).toBe(true)
+  })
 })
